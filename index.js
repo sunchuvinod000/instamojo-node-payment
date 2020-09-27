6a9f8e8f36e62deac18fcf3772875d1bb9267c87
sunchuvinod000 <sunchuvinod000@gmail.com>
2020-09-27 11:18:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const insta = require("instamojo-nodejs");
+const bodyparser = require("body-parser");
+const API_KEY= "test_dfef3ec888869f6db66902453b8";
+const AUTH_KEY= "test_be2f17e07d6eba0dd6b50904baf";
+
+
+const app = express()
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json())
+app.set('view engine','hbs');
+const port = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.post('/pay',(req,res)=>{
+    insta.setKeys(API_KEY, AUTH_KEY);
+    const name= req.body.name;
+    const email = req.body.email;
+    const amount = req.body.amount;
+    console.log(amount+name+email)
+    
+})
+
+
+
+
+ app.listen(port,()=>{
+     console.log("listeing");
+ })
